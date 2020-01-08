74115f525a6abaa42aef6c5d01f43123c9ccfa48
Sonia Kandah <sonia.kandah@gmail.com>
2020-01-07 16:13:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 var express = require('express'),
-    app = express.createServer(express.logger()),
-    io = require('socket.io').listen(app),
-    routes = require('./routes');
+    app = express.createServer(),
+    io = require('socket.io').listen(app);
 
 // Configuration
 
 app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
     app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function() {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function() {
-    app.use(express.errorHandler());
 });
 
 // Heroku won't actually allow us to use WebSockets
@@ -41,7 +27,7 @@ app.listen(port, function() {
     );
 });
 
-app.get('/', routes.index);
+app.get('/', (req, res, next) => {res.send('Homepage');});
 
 var status = 'All is well.';
 
