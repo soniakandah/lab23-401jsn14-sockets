f6c8f5bcc3c4adfce298a70e0b0e9970d35a72b5
Sonia Kandah <sonia.kandah@gmail.com>
2020-01-07 16:22:44 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express = require('express');
-const app = express.createServer();
-const io = require('socket.io').listen(app);
+const app = express();
+const server = require('http').createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
 
 // Configuration
-
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(app.router);
-});
-
-io.configure(function() {
-    io.set('transports', ['xhr-polling']);
-    io.set('polling duration', 10);
-});
 
 // Routes
 
 var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
 app.listen(port, function() {
-    console.log(
-        'Express server listening on port %d in %s mode',
-        app.address().port,
-        app.settings.env,
-    );
+    console.log('Express server listening');
 });
 
 app.get('/', (req, res, next) => {
@@ -31,10 +19,10 @@ app.get('/', (req, res, next) => {
 
 var status = 'All is well.';
 
-io.sockets.on('connection', function(socket) {
-    io.sockets.emit('status', { status: status }); // note the use of io.sockets to emit but socket.on to listen
+io.on('connection', function(socket) {
+    io.emit('status', { status: status }); // note the use of io.sockets to emit but socket.on to listen
     socket.on('reset', function(data) {
         status = 'War is imminent!';
-        io.sockets.emit('status', { status: status });
+        io.emit('status', { status: status });
     });
 });
